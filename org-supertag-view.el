9b69b3709e01f937284f1b704dd74afef9a38b26
Yibie <gunshotbox@gmail.com>
2025-02-12 18:07:08 +0800

    feat(view): add field value display to tag view
    This commit enhances the tag view system by adding field value display:
    
    - Add field value columns to tag view table
    - Implement case-insensitive field name matching
    - Add comprehensive field value lookup system
    - Improve table formatting for better readability

--- org-supertag-view.el
+++ org-supertag-view.el
@@ -22,6 +22,10 @@ Otherwise, prompt for a tag using completion."
                                 nil t)))
         (org-supertag-view--show-content tag)))))
 
+;;----------------------------------------------------------------------
+;; Get Value at Point
+;;----------------------------------------------------------------------  
+
 (defun org-supertag-view--get-tag-name ()
   "Get tag name at point.
 Returns nil if no tag is found at point."
@@ -45,24 +49,58 @@ Returns nil if no tag is found at point."
 ;; Get related nodes for a tag
 ;;----------------------------------------------------------------------    
 
+(defun org-supertag-view--get-field-link (node-id field-name)
+  "Get field link for NODE-ID and FIELD-NAME.
+Performs case-insensitive search for field name."
+  (let ((field-name-upcase (upcase field-name))
+        result)
+    (maphash
+     (lambda (link-id link-props)
+       (when (and (string-match ":node-field:\\(.+\\)->\\(.+\\)$" link-id)
+                  (equal (match-string 1 link-id) node-id)
+                  (equal (upcase (match-string 2 link-id)) field-name-upcase))
+         (setq result link-props)))
+     org-supertag-db--link)
+    result))
+
 (defun org-supertag-view--get-related-nodes (tag)
   "Get nodes related to TAG.
-Returns a list of plists with properties :node, :type, and :date."
-  (let ((nodes '()))
+Returns a list of plists with properties :node, :type, :date and field values."
+  (let* ((nodes '())
+         (tag-def (org-supertag-tag-get tag))
+         (fields (plist-get tag-def :fields)))
+    (message "Debug - Tag definition: %S" tag-def)
     (maphash
      (lambda (link-id link-props)
        (when (and (string-match ":node-tag:.*->\\(.+\\)$" link-id)
                   (equal (match-string 1 link-id) tag))
          (when-let* ((node-id (plist-get link-props :from))
                     (node-props (gethash node-id org-supertag-db--object)))
-           (push (list :node (or (plist-get node-props :title)
-                                (format "Node %s" node-id))
-                      :type (or (plist-get node-props :todo-state) "Node")
-                      :date (format-time-string 
-                            "%Y-%m-%d"
-                            (or (plist-get node-props :created-at)
-                                (current-time))))
-                 nodes))))
+           ;; 获取所有字段值
+           (let ((field-values
+                  (mapcar (lambda (field)
+                          (let* ((field-name (plist-get field :name))
+                                (field-link (org-supertag-view--get-field-link 
+                                           node-id field-name))
+                                (value (when field-link 
+                                       (plist-get field-link :value))))
+                            (message "Debug - Field value lookup: node=%s field=%s link=%S value=%S" 
+                                    node-id field-name field-link value)
+                            (cons field-name value)))
+                        fields)))
+             (message "Debug - Node field values: %S" field-values)
+             (push (append
+                    (list :node (or (plist-get node-props :title)
+                                   (format "Node %s" node-id))
+                          :type (or (plist-get node-props :todo-state) "Node")
+                          :date (format-time-string 
+                                "%Y-%m-%d"
+                                (or (plist-get node-props :created-at)
+                                    (current-time)))
+                          :id node-id
+                          :fields field-values)
+                    node-props)
+                   nodes)))))
      org-supertag-db--link)
     (nreverse nodes)))
 
@@ -82,19 +120,50 @@ Returns a list of plists with properties :node, :type, and :date."
   "Insert header for TAG view."
   (insert (format "Content tagged with #%s:\n\n" tag)))
 
+(defun org-supertag-view--get-field-value (node-id field-name tag-id)
+  "Get field value for NODE-ID, FIELD-NAME and TAG-ID.
+Returns the field value or nil if not found."
+  (let* ((link-id (format ":node-field:%s->%s" node-id field-name))
+         (link (gethash link-id org-supertag-db--link)))
+    (message "Debug - Getting field value: node=%s field=%s tag=%s link=%S"
+             node-id field-name tag-id link)
+    (when (and link 
+               (equal (plist-get link :tag-id) tag-id))
+      (plist-get link :value))))
+
 (defun org-supertag-view--insert-content (tag)
   "Insert content related to TAG in current buffer."
-  (let ((content (org-supertag-view--get-related-nodes tag)))
+  (insert "* Related Nodes\n\n")
+  (let* ((content (org-supertag-view--get-related-nodes tag))
+         (tag-def (org-supertag-tag-get tag))
+         (fields (plist-get tag-def :fields)))
+    (message "Debug - Content: %S" content)
     (if content
         (progn
-          (insert "| Node | Type | Date |\n")
-          (insert "|------+------+------|\n")
+          ;; 构建表头
+          (insert "|Node|Type|Date|")
+          (dolist (field fields)
+            (insert (format "%s|" (plist-get field :name))))
+          (insert "\n|-\n")
+          
+          ;; 插入每个节点的数据
           (dolist (item content)
-            (insert (format "| %s | %s | %s |\n"
-                          (plist-get item :node)
-                          (plist-get item :type)
-                          (plist-get item :date))))
-          (org-table-align))
+            (let ((field-values (plist-get item :fields)))
+              (insert "|")
+              (insert (format "%s|%s|%s|"
+                            (plist-get item :node)
+                            (plist-get item :type)
+                            (plist-get item :date)))
+              ;; 插入字段值
+              (dolist (field fields)
+                (let* ((field-name (plist-get field :name))
+                       (value (cdr (assoc field-name field-values))))
+                  (insert (format "%s|" (or value "")))))
+              (insert "\n")))
+          (insert "\n")
+          (save-excursion
+            (forward-line -1)
+            (org-table-align)))
       (insert (format "No content found for tag #%s" tag)))))
 
 (define-derived-mode org-supertag-view-mode org-mode "SuperTag-View"
